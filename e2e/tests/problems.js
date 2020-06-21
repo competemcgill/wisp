c5cc1b29ae06eb0410ea943e0b241abbadad19ed
Ahmed Elehwany <ahmed.elehwany@mail.mcgill.ca>
2020-06-21 16:36:34 -0400

    Implement dockerized tests for chrome and firefox

--- e2e/tests/problems.js
+++ e2e/tests/problems.js
@@ -2,6 +2,6 @@ var config = require('../nightwatch.conf.js');
 
 module.exports = {
 	'User should be able to view list of problems' : function(browser) {
-		<!-- TODO: Write test here-->
+		// TODO: Write test here
 	}
 };
